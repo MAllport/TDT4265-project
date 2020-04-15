9ef3db0b18639e9882deeb4c8c0323499d10c6a8
jennybg <jennybg@stud.ntnu.no>
2020-04-15 11:34:46 +0000

    model works with resnet50,101,152

--- SSD/ssd/modeling/backbone/resnet50.py
+++ SSD/ssd/modeling/backbone/resnet50.py
@@ -17,30 +17,193 @@ class Resnet50(torch.nn.Module):
         
         backbone = models.resnet50(pretrained=True)
 
-        """
-        self.feature1 = nn.Sequential(backbone.conv1, backbone.bn1, backbone.relu, backbone.maxpool) # out 64
-        self.feature2 = backbone.layer1 # out 256
-        self.feature3 = backbone.layer2 # out 512
-        self.feature4 = backbone.layer3 # out 1024
-        self.feature5 = backbone.layer4 # out 2048
-
-
-        
-        feature_extractor = [self.feature1, self.feature2, self.feature3, self.feature4, self.feature5]
-        self.feature_extractor = nn.ModuleList(feature_extractor)
-        
-        """
-        self.feature_extractor = nn.Sequential(*list(backbone.children())[:7])
-
-        conv4_block1 = self.feature_extractor[-1][0]
+        # out of bank1 -> 1024 x 38 x 38
+        # source https://github.com/NVIDIA/DeepLearningExamples/blob/master/PyTorch/Detection/SSD/src/model.py
+        self.bank1 = nn.Sequential(*list(backbone.children())[:7])
+        conv4_block1 = self.bank1[-1][0]
         conv4_block1.conv1.stride = (1,1)
         conv4_block1.conv2.stride = (1,1)
         conv4_block1.downsample[0].stride = (1,1)
+        
+        """
+        # LITT MER EXTRA EXTRA FEATURE LAYERS
+        # out of bank2 -> 512 x 19 x 19
+        self.bank2 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = self.output_channels[0],
+                out_channels = 1024,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.MaxPool2d(
+                kernel_size = 2, 
+                stride = 2
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = 1024,
+                out_channels = self.output_channels[1],
+                kernel_size=3,
+                stride=1,
+                padding=1
+            )
+        )
+        # out -> 512 x 10 x 10
+        self.bank3 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = output_channels[1],
+                out_channels = 1024,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = 1024,
+                out_channels = 512,
+                kernel_size=3,
+                stride=2,
+                padding=1
+            )
+        )
+        # out -> 256 x 5 x 5
+        self.bank4 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = output_channels[2],
+                out_channels = 512,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = 512,
+                out_channels = output_channels[3],
+                kernel_size=3,
+                stride=2,
+                padding=1
+            )
+        )
+        # out of bank5 -> 256 x 3 x 3
+        self.bank5 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = output_channels[3],
+                out_channels = 512,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = 512,
+                out_channels = output_channels[4],
+                kernel_size=3,
+                stride=2,
+                padding=1
+            )
+        )
+        # out of bank6 -> 128 x 1 x 1
+        self.bank6 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = self.output_channels[4],
+                out_channels = 256,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = 256,
+                out_channels = self.output_channels[5],
+                kernel_size=3,
+                stride=1,
+                padding=0
+            )
+        )
+        """
+
+        # HELT BASIC EXTRA FEATURE LAYERS
+        # out of bank2 -> 512 x 19 x 19
+        self.bank2 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = self.output_channels[0],
+                out_channels = self.output_channels[1],
+                kernel_size=3,
+                stride=2,
+                padding=1
+            )
+        )
+        # out -> 512 x 10 x 10
+        self.bank3 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = self.output_channels[1],
+                out_channels = self.output_channels[2],
+                kernel_size=3,
+                stride=2,
+                padding=1
+            )
+        )
+        # out -> 256 x 5 x 5
+        self.bank4 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = self.output_channels[2],
+                out_channels = self.output_channels[3],
+                kernel_size=3,
+                stride=2,
+                padding=1
+            )
+        )
+        # out of bank5 -> 256 x 3 x 3
+        self.bank5 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = self.output_channels[3],
+                out_channels = self.output_channels[4],
+                kernel_size=3,
+                stride=2,
+                padding=1
+            )
+        )
+        # out of bank6 -> 128 x 1 x 1
+        self.bank6 = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels = self.output_channels[4],
+                out_channels = self.output_channels[5],
+                kernel_size=3,
+                stride=1,
+                padding=0
+            )
+        )
 
 
-        print("FEATURE EXTRACTOR")
-        print(self.feature_extractor)
+        print("BANK 1")
+        print(self.bank1)
+        print("BANK 2")
+        print(self.bank2)
+        print("BANK 3")
+        print(self.bank3)
+        print("BANK 4")
+        print(self.bank4)
+        print("BANK 5")
+        print(self.bank5)
+        print("BANK 6")
+        print(self.bank6)
+
+        self.feature_extractor = nn.ModuleList([self.bank1, self.bank2, self.bank3, self.bank4, self.bank5, self.bank6])
+
     
+
     def forward(self, x):
         
         out_features = []
@@ -48,12 +211,12 @@ class Resnet50(torch.nn.Module):
         for feature in self.feature_extractor:
             x = feature(x)
             out_features.append(x)
-            print("SIZE OF FEATURE: ", x.shape)
-        
+
+        """
         for idx, feature in enumerate(out_features):
             expected_shape = (out_channel, feature_map_size, feature_map_size)
             assert feature.shape[1:] == expected_shape, \
                 f"Expected shape: {expected_shape}, got: {feature.shape[1:]} at output IDX: {idx}"
-        
+        """
         return tuple(out_features)
         
